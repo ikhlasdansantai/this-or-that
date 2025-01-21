f214fe5eab1b341605fe1f0cc4552b5d2bd54a6b
ikhlasdansantai <initesting5@gmail.com>
2025-01-21 11:54:50 +0700

    chore: add static data file for quiz functionality

---
+++ src/static/data.ts
@@ -0,0 +1,26 @@
+export const members = [
+  {
+    name: 'chaewon',
+    image: 'https://i.pinimg.com/736x/bb/67/60/bb676035bb34a641c900faa3db94a7a3.jpg',
+  },
+  {
+    name: 'hyein',
+    image: 'https://i.pinimg.com/736x/62/a7/11/62a711a4657bdca09a3c9493b271f744.jpg',
+  },
+  {
+    name: 'winter',
+    image: 'https://i.pinimg.com/736x/d4/34/1c/d4341c9a54f8ee8ea5cde729268857ae.jpg',
+  },
+  {
+    name: 'haerin',
+    image: 'https://i.pinimg.com/736x/2a/bb/5c/2abb5c307afade3463144bb329c0300f.jpg',
+  },
+  {
+    name: 'hanni',
+    image: 'https://i.pinimg.com/736x/03/47/8d/03478da49bfb7c3bddd73bbdb91abf45.jpg',
+  },
+  {
+    name: 'giselle',
+    image: 'https://i.pinimg.com/736x/84/2c/97/842c97aaeb62d3c7f1b7e2f3831ba36a.jpg',
+  },
+]

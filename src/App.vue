8b18059bad3040649c8005e929681a698d70cdb5
ikhlasdansantai <initesting5@gmail.com>
2025-01-22 11:50:14 +0700

    ref: clean up code and modularize components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { members } from '@/static/data'
+import type { Member } from '@/static/type'
+import MemberList from '@/components/ui/MemberList.vue'
+import PickedMemberCard from '@/components/ui/PickedMemberCard.vue'
+import ButtonItem from '@/components/ui/ButtonItem.vue'
+import HeaderItem from '@/components/ui/HeaderItem.vue'
 
 const selectedMembers = ref([members[0], members[1]])
 const index = ref<number>(1)
 const isFinished = ref<boolean>(false)
-const choosenMember = ref<string | undefined>(undefined)
+const choosenMember = ref<Member | null>(null)
 
 const getChoose = (choose: string) => {
-  const unChooseMember = members.filter((c) => !c.name.includes(choose))
+  const unChooseMember = members.filter((member) => !member.name.includes(choose))
 
-  console.log(index.value)
-  console.log(members.length - 1)
-  choosenMember.value = choose
+  const finalMember = members.find((member) => member.name === choose)
+  if (finalMember) choosenMember.value = finalMember
 
-  if (index.value === members.length - 1) {
-    alert('Ur final choice is ' + choose)
-    isFinished.value = true
-    return
-  }
+  if (index.value === members.length - 1) isFinished.value = true
 
-  if (selectedMembers.value[0].name === choose) {
+  if (selectedMembers.value[0].name === choose)
     selectedMembers.value[1] = unChooseMember[index.value]
-  } else if (selectedMembers.value[1].name === choose) {
+  else if (selectedMembers.value[1].name === choose)
     selectedMembers.value[0] = unChooseMember[index.value]
-  } else return
+  else return
 
   index.value++
 }
@@ -33,51 +33,28 @@ const reset = () => {
   index.value = 1
   selectedMembers.value = [members[0], members[1]]
   isFinished.value = false
-  choosenMember.value = undefined
-  return
+  choosenMember.value = null
 }
 </script>
 
 <template>
-  <header id="container-all" class="space-y-2">
-    <h1 class="text-center font-sans text-4xl font-semibold">
-      This <span class="text-xl">or</span> That
-    </h1>
-    <p class="text-center">Choose between two options by clicking on the image.</p>
+  <header class="space-y-2">
+    <header-item :is-finished />
   </header>
 
   <main>
-    <section class="mt-10 grid w-full gap-10 sm:grid-cols-2">
-      <ul id="choose-wrapper" v-for="member in selectedMembers" :key="member.name">
-        <li
-          id="choose-card"
-          class="aspect-square w-full cursor-pointer rounded-md border pb-4 text-center transition-all duration-300 hover:shadow-md"
-          @click="getChoose(member.name)"
-          :class="{
-            'pointer-events-none opacity-80': isFinished,
-            'border-4 border-green-400': choosenMember === member.name,
-          }"
-        >
-          <figure class="overflow-hidden rounded-md p-4">
-            <img
-              :src="member.image"
-              :alt="`${member.name}-profile-image`"
-              class="block aspect-square max-w-full object-cover"
-            />
-          </figure>
-          <h5>{{ member.name }}</h5>
-        </li>
-      </ul>
+    <section class="mt-10">
+      <member-list
+        v-if="!isFinished"
+        :selected-members
+        :is-finished
+        :choosen-member
+        @get-choose="getChoose"
+      />
+      <picked-member-card v-else :choosen-member />
     </section>
 
-    <button
-      @click="reset"
-      :disabled="choosenMember === undefined"
-      :class="{ 'pointer-events-none opacity-50': choosenMember === undefined }"
-      class="mx-auto mt-10 block rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
-    >
-      Reset
-    </button>
+    <button-item :choosen-member="choosenMember" @reset="reset" />
   </main>
 </template>
 
---
+++ src/components/ui/ButtonItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    @click="handleReset"
+    :disabled="isDisabled"
+    :class="disabledClass"
+    class="mx-auto mt-10 block rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
+  >
+    Reset
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { Member } from '@/static/type'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  choosenMember: Member | null
+}>()
+const isDisabled = computed(() => props.choosenMember === null)
+const disabledClass = computed(() => isDisabled.value && 'pointer-events-none opacity-50')
+
+const emit = defineEmits<{ (event: 'reset'): void }>()
+const handleReset = () => emit('reset')
+</script>
